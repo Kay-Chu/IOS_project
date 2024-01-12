c6f41d54ecfce89e599abf18a306b2905ec907e1
Kaying <39971215+Kay-Chu@users.noreply.github.com>
2024-01-12 17:15:50 +0800

    Initial Commit

---
+++ IOS_project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS_project
+//
+//  Created by KA YING CHU on 12/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS_project/IOS_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_projectApp.swift
+//  IOS_project
+//
+//  Created by KA YING CHU on 12/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOS_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
